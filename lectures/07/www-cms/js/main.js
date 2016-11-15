fc687237ce47043fcb9b0e9cef811e8f714c22cb
hannvanh5 <hannes.vanhijfte@student.arteveldehs.be>
2016-11-15 11:20:37 +0100

    cms

---
+++ lectures/07/www-cms/js/main.js
@@ -0,0 +1,17 @@
+function ready(cb) {
+    /in/.test(document.readyState)
+    ? setTimeout(ready.bind(null, cb), 90)
+    : cb();
+};
+
+ready(function(){
+
+    var App = {
+        "init": function() {
+            this._applicationDbContext = ApplicationDbContext; // Reference to the ApplicationDbContext object
+            this._applicationDbContext.init('ahs.dds.cms'); // Intialize the ApplicationDbContext with the connection string as parameter value
+        }
+    };
+
+    App.init();
+});
